dbfae55b6dfa43ac0a08454d36b550f013e354f9
vitterre <bagatovbb@gmail.com>
2023-05-06 14:48:36 +0300

    feat: add draft on product page

--- src/components/SGButton.vue
+++ src/components/SGButton.vue
@@ -1,10 +1,22 @@
-<script setup></script>
+<script setup>
+defineProps({
+  available: Boolean,
+  altText: String
+})
+</script>
 
 <template>
   <button
+    v-if="available"
     class="bg-yellow-300 hover:bg-yellow-400 active:bg-yellow-300 text-stone-900 py-2 px-6 transition duration-300 rounded"
   >
     <slot />
+  </button>
+  <button
+    v-else
+    class="bg-stone-300 text-stone-900 py-2 px-6 rounded cursor-not-allowed"
+  >
+    {{ altText }}
   </button>
 </template>
 
---
+++ src/views/ProductView.vue
@@ -0,0 +1,45 @@
+<template>
+  <main class="max-w-[1200px] p-4 flex">
+    <div class="w-[65%] relative">
+      <img :src="images[0]" class="w-96" alt="">
+    </div>
+    <div class="w-[35%]">
+      <div>
+        <h1 class="font-bold text-lg">{{ name }}</h1>
+        <p class="mt-5">
+          <pre>{{ description }}</pre>
+        </p>
+      </div>
+      <div class="mt-5">
+        <p class="font-bold">${{ price }}</p>
+        <!-- <SGButton v-if="amount">Add to cart</SGButton> -->
+        <SGButton class="mt-5" :available="amount > 0" alt-text="Out of stock">Add to cart</SGButton>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import SGButton from '../components/SGButton.vue'
+
+export default {
+  components: {
+    SGButton
+  },
+  data() {
+    return {
+      images: [
+        'https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/auto_image/cache=expiry:max/rotate=deg:exif/resize=height:1400/output=quality:50/no_metadata/compress/2yri3mroQkquV0ghSbe3',
+        'https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/auto_image/cache=expiry:max/rotate=deg:exif/resize=height:1400/output=quality:50/no_metadata/compress/5t6jDwLVTi2lPRv5UbzO',
+        'https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/auto_image/cache=expiry:max/rotate=deg:exif/resize=height:1400/output=quality:50/no_metadata/compress/4yiOI4tLSqWUYDUCBzF6',
+        'https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/auto_image/cache=expiry:max/rotate=deg:exif/resize=height:1400/output=quality:50/no_metadata/compress/wwtYASDkS0uMfkMZWdIm',
+        'https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/auto_image/cache=expiry:max/rotate=deg:exif/resize=height:1400/output=quality:50/no_metadata/compress/DA7sCaSQTKyldZjDTrQw'
+      ],
+      name: 'Nowhereman Tee Red',
+      description: 'Nowhereman red t-shirt by Number (N)ine.\nSize Men\'s / US S / EU 44-46 / 1\nColor Red\nCondition New',
+      price: '69.90',
+      amount: 22
+    }
+  }
+}
+</script>
