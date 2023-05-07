b1d4d3efd7157654b82a4321fee139b10a883719
timergOoOt <timur18092004@gmail.com>
2023-05-07 11:45:03 +0300

    fx: fixed problems with displaying information on the card

--- src/components/SGCard.vue
+++ src/components/SGCard.vue
@@ -10,13 +10,13 @@ defineProps({
 <template>
 	<card class="">
 		<div class="bg-stone-200 w-52 h-68 m-5 py-2 rounded hover:bg-stone-50 active:bg-stone-50 transition duration-700 ">
-			<div class="bg-white w-48 h-44 mr-2 ml-2 rounded p-12">
-				<img :src="image" alt="" srcset="">
-			</div>
+			<div class="bg-white w-48 h-44 mr-2 ml-2 rounded"><img class="object-cover bg-no-repeat bg-center w-full h-full rounded" src=""  alt="Picture"></div>
 			<div class="text-stone-950 text-center">
-				<h2 class="text-lg sg-logo-text pt-1">{{ title }}</h2>
-				<h2 class="sg-logo-text pb-2">${{ price }}</h2>
-				<div class="sg-nav-link font-extralight text-sm hover:text-stone-600 active:text-stone-500">{{ description }}</div>
+				<div class="sg-logo-text text-lg clear-both mx-3">
+					<h2 class="float-left">TITLE</h2>
+					<h2 class="float-right">$999</h2>
+				</div>	
+				<div class="sg-nav-link font-extralight text-sm hover:text-stone-600 active:text-stone-500 text-justify mx-3">Description</div>
 			</div>
 		</div>
 	</card>
