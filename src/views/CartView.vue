ee29ec1b33201e7a4e77b01a87f249f41e32d944
vitterre <bagatovbb@gmail.com>
2023-05-11 12:28:12 +0300

    feat: add placeholder text if there is no products in the cart

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,7 +1,7 @@
 <template>
-	<main class="max-w-[1200px] p-4 ml-8 mr-10 text pb-12">
-		<div class="text-2xl font-bold">Your cart</div>
-		<div class="w-[80%]">
+	<main class="max-w-[1200px] ml-16 mr-10 text pb-12 min-h-[400px]">
+		<div class="text-2xl font-bold sg-logo-text">Your cart</div>
+		<div class="w-[80%]" v-if="products.length > 0">
 			<div class="products pt-5 pb-10">
 					<div class="product w-full inline-table" v-for="product in products" v-bind:key="product">
 						<div class="text-lg float-left w-[80%]"><div class="text-lg max-w-[40%] float-left overflow-ellipsis overflow-hidden whitespace-nowrap">{{ product.name }}</div></div>
@@ -11,6 +11,7 @@
 			</div>
 			<SGButton class="w-[20%] min-w-[100px] align-middle text-center absolute right-0 font-semibold">Checkout</SGButton>
 		</div>
+		<p class="text-base mt-5" v-else>You have no items in your cart. Continue shopping.</p>
 	</main>
   </template>
   
@@ -30,73 +31,74 @@
 		  'https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/auto_image/cache=expiry:max/rotate=deg:exif/resize=height:1400/output=quality:50/no_metadata/compress/wwtYASDkS0uMfkMZWdIm',
 		  'https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/auto_image/cache=expiry:max/rotate=deg:exif/resize=height:1400/output=quality:50/no_metadata/compress/DA7sCaSQTKyldZjDTrQw'
 		],
-		products: [
-				{
-					id: 1,
-					category_id: 1,
-					name: 'Number (N)ine Destroyed Tee',
-					description: 'This t-shirt has a thrashed vintage look with an illusion of dirt scattering across the shirt.',
-					price: '0',
-					amount:5,
-					images: [
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-1-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-4-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-2-min_2.jpg?v=1659901749'
-					]
-				},
-				{
-					id: 1,
-					category_id: 1,
-					name: 'Number (N)ine Destroyed Tee',
-					description: 'This t-shirt has a thrashed vintage look with an illusion of dirt scattering across the shirt.',
-					price: '606,95',
-					amount: 0,
-					images: [
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-1-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-4-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-2-min_2.jpg?v=1659901749'
-					]
-				},
-				{
-					id: 1,
-					category_id: 1,
-					name: 'Number (N)ine Destroyed Tee',
-					description: 'This t-shirt has a thrashed vintage look with an illusion of dirt scattering across the shirt.',
-					price: '606,95',
-					amount: 2,
-					images: [
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-1-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-4-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-2-min_2.jpg?v=1659901749'
-					]
-				},
-				{
-					id: 1,
-					category_id: 1,
-					name: 'Number (N)ine Destroyed Tee dcjgdcudcdgcduvdc',
-					description: 'This t-shirt has a thrashed vintage look with an illusion of dirt scattering across the shirt.',
-					price: '606,95',
-					amount: 17444,
-					images: [
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-1-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-4-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-2-min_2.jpg?v=1659901749'
-					]
-				},
-				{
-					id: 1,
-					category_id: 1,
-					name: 'Number (N)ine Destroyed Tee',
-					description: 'This t-shirt has a thrashed vintage look with an illusion of dirt scattering across the shirt.',
-					price: '60,95',
-					amount: 22,
-					images: [
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-1-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-4-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-2-min_2.jpg?v=1659901749'
-					]
-				}
-			]
+		// products: [
+		// 		{
+		// 			id: 1,
+		// 			category_id: 1,
+		// 			name: 'Number (N)ine Destroyed Tee',
+		// 			description: 'This t-shirt has a thrashed vintage look with an illusion of dirt scattering across the shirt.',
+		// 			price: '0',
+		// 			amount:5,
+		// 			images: [
+		// 				'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-1-min_2.jpg?v=1659901749',
+		// 				'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-4-min_2.jpg?v=1659901749',
+		// 				'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-2-min_2.jpg?v=1659901749'
+		// 			]
+		// 		},
+		// 		{
+		// 			id: 1,
+		// 			category_id: 1,
+		// 			name: 'Number (N)ine Destroyed Tee',
+		// 			description: 'This t-shirt has a thrashed vintage look with an illusion of dirt scattering across the shirt.',
+		// 			price: '606,95',
+		// 			amount: 0,
+		// 			images: [
+		// 				'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-1-min_2.jpg?v=1659901749',
+		// 				'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-4-min_2.jpg?v=1659901749',
+		// 				'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-2-min_2.jpg?v=1659901749'
+		// 			]
+		// 		},
+		// 		{
+		// 			id: 1,
+		// 			category_id: 1,
+		// 			name: 'Number (N)ine Destroyed Tee',
+		// 			description: 'This t-shirt has a thrashed vintage look with an illusion of dirt scattering across the shirt.',
+		// 			price: '606,95',
+		// 			amount: 2,
+		// 			images: [
+		// 				'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-1-min_2.jpg?v=1659901749',
+		// 				'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-4-min_2.jpg?v=1659901749',
+		// 				'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-2-min_2.jpg?v=1659901749'
+		// 			]
+		// 		},
+		// 		{
+		// 			id: 1,
+		// 			category_id: 1,
+		// 			name: 'Number (N)ine Destroyed Tee dcjgdcudcdgcduvdc',
+		// 			description: 'This t-shirt has a thrashed vintage look with an illusion of dirt scattering across the shirt.',
+		// 			price: '606,95',
+		// 			amount: 17444,
+		// 			images: [
+		// 				'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-1-min_2.jpg?v=1659901749',
+		// 				'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-4-min_2.jpg?v=1659901749',
+		// 				'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-2-min_2.jpg?v=1659901749'
+		// 			]
+		// 		},
+		// 		{
+		// 			id: 1,
+		// 			category_id: 1,
+		// 			name: 'Number (N)ine Destroyed Tee',
+		// 			description: 'This t-shirt has a thrashed vintage look with an illusion of dirt scattering across the shirt.',
+		// 			price: '60,95',
+		// 			amount: 22,
+		// 			images: [
+		// 				'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-1-min_2.jpg?v=1659901749',
+		// 				'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-4-min_2.jpg?v=1659901749',
+		// 				'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-2-min_2.jpg?v=1659901749'
+		// 			]
+		// 		}
+		// 	]
+		products: []
 	  }
 	}
   }
