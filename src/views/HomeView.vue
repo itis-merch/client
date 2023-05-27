fee4b9aa1d5912194faeca2cf39ac8b7f688e2d1
volokitty <v.volokitty@gmail.com>
2023-05-27 18:05:20 +0300

    feat: grid cols

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
         <!-- <SGCarousel :slides="images" class="mb-10 mx-10"/> -->
         <div class="new-arrivals">
             <h1 class="font-bold">New Arrivals</h1>
-            <div class="products flex flex-row">
+            <div class="products grid grid-cols-3">
                 <div class="product" v-for="product in products" v-bind:key="product">
                     <SGCard :title="product.title" :description="product.description" :image="product.images[0]" :price="product.price" />
                 </div>
