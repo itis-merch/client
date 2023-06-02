0b15df3d5e83d82ee470af25a2b6307f1bb45c6b
timergOoOt <timur18092004@gmail.com>
2023-06-02 10:50:08 +0300

    feat: add get request and cart authorization

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -6,7 +6,7 @@
 					<div class="product w-full inline-table" v-for="product in products" v-bind:key="product">
 						<div class="text-lg float-left w-[80%]"><div class="text-lg max-w-[40%] float-left overflow-ellipsis overflow-hidden whitespace-nowrap">{{ product.name }}</div></div>
 						<div class="text-lg text-right float-left"> {{ product.amount }}</div>
-						<div class="font-bold text-base float-right">${{ product.price }}</div>
+						<div class="font-bold text-base float-right">${{ product.totalPrice }}</div>
 					</div>
 			</div>
 			<SGButton class="w-[20%] min-w-[100px] align-middle text-center absolute right-0 font-semibold">Checkout</SGButton>
@@ -17,13 +17,47 @@
   
   <script>
 	import SGButton from '../components/SGButton.vue'
+	import axios from 'axios'
   
   export default {
+	mounted() {
+		try {
+			
+			axios.get(this.baseURL + "cart", {
+				headers: {
+					Authorization: 'Bearer ' + this.jwtToken
+				}
+			})
+			.then((response) => {
+				this.cart = response.data.products;
+				console.log(this.cart);
+				for(var i = 0; i < this.cart.length; i++) {
+					this.product.amount = this.cart[i].quantity;
+
+					axios.get(this.baseURL + "products/" + this.cart[i].product_id)
+					.then((response) => {
+						const data = response.data;
+						this.product.name = data.name;
+						this.product.totalPrice = data.price * this.product.amount;
+					})
+
+					this.products.push(this.product);
+				}
+			})
+		} catch(error) {
+			console.log(error);
+		}
+	},
+
 	components: {
 		SGButton
 	},
 	data() {
 	  return {
+
+		jwtToken: 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJpdmFud2hlZWxAZ21haWwuY29tIiwicm9sZSI6IkFETUlOIiwiZXhwIjoxNjg1NjkyNzAzLCJpYXQiOjE2ODU2OTE1MDN9.LA24JVmG4AK4LiVmIriB-MmzTD_RAw8CKDwRoJSnUmE',
+		baseURL: 'http://45.9.73.210:8080/api/v1/',
+		cart: null,
 		images: [
 		  'https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/auto_image/cache=expiry:max/rotate=deg:exif/resize=height:1400/output=quality:50/no_metadata/compress/2yri3mroQkquV0ghSbe3',
 		  'https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/auto_image/cache=expiry:max/rotate=deg:exif/resize=height:1400/output=quality:50/no_metadata/compress/5t6jDwLVTi2lPRv5UbzO',
@@ -98,7 +132,12 @@
 		// 			]
 		// 		}
 		// 	]
-		products: []
+		products: [], 
+		product: {
+			name: '',
+			amount: 0, 
+			totalPrice: 0
+		}
 	  }
 	}
   }
