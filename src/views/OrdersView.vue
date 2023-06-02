4a4a09fa3675a09f893b424a5f63484dce65e0fd
Eldar <bef1eldar@gmail.com>
2023-06-02 18:10:58 +0300

    feat: add receiving orders from the backend

--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -1,11 +1,11 @@
-<template>
+	<template>
 	<main class="max-w-[1200px] ml-16 mr-10 pb-12 min-h-[400px]">
 		<div class="text-2xl font-bold sg-logo-text">Your orders</div>
 		<div class="products pt-5 w-[80%]" v-if="products.length > 0">
 				<div class="product w-full inline-table" v-for="product in products" v-bind:key="product">
-					<div class="text-lg float-left w-[80%]"><div class="text-lg max-w-[40%] float-left overflow-ellipsis overflow-hidden whitespace-nowrap">{{ product.name }}</div></div>
-					<div class="text-lg text-right float-left"> {{ product.amount }}</div>
-					<div class="font-bold text-base float-right">${{ product.price }}</div>
+					<div class="text-lg float-left w-[10%]"><div class="text-lg max-w-[40%] float-left overflow-ellipsis overflow-hidden whitespace-nowrap">{{ product.id }}</div></div>
+					<div class="text-lg text-right float-left"> {{ product.products }}</div>
+					<div class="font-bold text-base float-right">${{ product.cost }}</div>
 				</div>
 		</div>
 		<p class="mt-4 text-base " v-else>You have no orders yet. Continue shopping.</p>
@@ -13,88 +13,45 @@
   </template>
   
   <script>
-
-  
+	import axios from "axios";
   export default {
-	components: {
-	},
-	data() {
-	  return {
-		images: [
-		  'https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/auto_image/cache=expiry:max/rotate=deg:exif/resize=height:1400/output=quality:50/no_metadata/compress/2yri3mroQkquV0ghSbe3',
-		  'https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/auto_image/cache=expiry:max/rotate=deg:exif/resize=height:1400/output=quality:50/no_metadata/compress/5t6jDwLVTi2lPRv5UbzO',
-		  'https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/auto_image/cache=expiry:max/rotate=deg:exif/resize=height:1400/output=quality:50/no_metadata/compress/4yiOI4tLSqWUYDUCBzF6',
-		  'https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/auto_image/cache=expiry:max/rotate=deg:exif/resize=height:1400/output=quality:50/no_metadata/compress/wwtYASDkS0uMfkMZWdIm',
-		  'https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/auto_image/cache=expiry:max/rotate=deg:exif/resize=height:1400/output=quality:50/no_metadata/compress/DA7sCaSQTKyldZjDTrQw'
-		],
-		products: [
-				{
-					id: 1,
-					category_id: 1,
-					name: 'Number (N)ine Destroyed Tee',
-					description: 'This t-shirt has a thrashed vintage look with an illusion of dirt scattering across the shirt.',
-					price: '0',
-					amount:5,
-					images: [
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-1-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-4-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-2-min_2.jpg?v=1659901749'
-					]
-				},
-				{
-					id: 1,
-					category_id: 1,
-					name: 'Number (N)ine Destroyed Tee',
-					description: 'This t-shirt has a thrashed vintage look with an illusion of dirt scattering across the shirt.',
-					price: '606,95',
-					amount: 0,
-					images: [
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-1-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-4-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-2-min_2.jpg?v=1659901749'
-					]
-				},
-				{
-					id: 1,
-					category_id: 1,
-					name: 'Number (N)ine Destroyed Tee',
-					description: 'This t-shirt has a thrashed vintage look with an illusion of dirt scattering across the shirt.',
-					price: '606,95',
-					amount: 2,
-					images: [
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-1-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-4-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-2-min_2.jpg?v=1659901749'
-					]
-				},
-				{
-					id: 1,
-					category_id: 1,
-					name: 'Number (N)ine Destroyed Tee dcjgdcudcdgcduvdc',
-					description: 'This t-shirt has a thrashed vintage look with an illusion of dirt scattering across the shirt.',
-					price: '606,95',
-					amount: 17444,
-					images: [
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-1-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-4-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-2-min_2.jpg?v=1659901749'
-					]
-				},
-				{
-					id: 1,
-					category_id: 1,
-					name: 'Number (N)ine Destroyed Tee',
-					description: 'This t-shirt has a thrashed vintage look with an illusion of dirt scattering across the shirt.',
-					price: '60,95',
-					amount: 22,
-					images: [
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-1-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-4-min_2.jpg?v=1659901749',
-						'https://cdn.shopify.com/s/files/1/0614/0972/2528/products/NN-DESTROYED-TEE-2-min_2.jpg?v=1659901749'
-					]
+		components: {},
+		data() {
+			return {
+				baseUrl: "http://45.9.73.210:8080/api/v1/",
+				jwt_token: "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJpdmFud2hlZWxAZ21haWwuY29tIiwicm9sZSI6IkFETUlOIiwiZXhwIjoxNjg1NzE5NjY0LCJpYXQiOjE2ODU3MTg0NjR9.XY2o34WeJa30U7c48843crzQmfs6q4q4i4UO_FaTJIQ",
+				products: []
+			}
+		},
+		async mounted() {
+			await axios.get(this.baseUrl + "cart/order", {
+				headers: {
+					'Authorization': `Bearer ${this.jwt_token}`
 				}
-			]
-	  }
+			})
+					.then(async (response) => {
+						for (let i = 0; i < response.data.length; i++) {
+							let productId = response.data[i].id
+							let products = "";
+							let cost = response.data[i].total_price
+							for (let j = 0; j < response.data[i].products.length; j++) {
+								let name = ""
+								await axios.get(this.baseUrl + "products/" + (j + 1))
+										.then((response) => {
+											name = response.data.name
+										})
+								if (j != (response.data[i].products.length - 1))
+									products += name + ", "
+								else
+									products += name
+							}
+							this.products.push({
+								id: productId,
+								products: products,
+								cost: cost
+							})
+						}
+					})
+		}
 	}
-  }
   </script>
